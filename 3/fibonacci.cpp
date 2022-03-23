4966d655c4dae11c951d78e79a78c893d2a7d644
ivanVillalobs <a21310394@ceti.mx>
2022-03-23 16:59:16 -0600

    Serie fibonacci que muestra los primeros 20 numeros de la misma.

---
+++ 3/fibonacci.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int num = 1, anterior = 0, aux;
+    for (int i = 0; i < 20; i++) {
+        cout << " " << num << endl;
+        aux = num;
+        num += anterior;
+        anterior = aux;
+    }
+    
+    cout << "\n\n"<<endl;
+    
+
+    system("pause");
+    return 0;
+}
+
