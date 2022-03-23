911f76df7b86c87bb92d26f97501afa4fdbafe86
ivanVillalobs <a21310394@ceti.mx>
2022-03-23 16:05:57 -0600

    programa que saca pares de 2 hasta n numero

--- 1/Programa1.cpp
+++ 1/Programa1.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-
+	int i;
+	cout << "ingrece el numero par hasta el que desea llegar \n" << endl;
+	cin >> i;
+	for (int a = 2; a <= i;a+=2) {
+		cout << a <<"\n";
+	}
+	return 0;
 }
