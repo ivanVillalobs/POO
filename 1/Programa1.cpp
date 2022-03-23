12075ffbaac8bfa171a904b72286566498511210
ivanVillalobs <a21310394@ceti.mx>
2022-03-23 15:58:02 -0600

    borre lo de prueba para hacer el primer programa

--- 1/Programa1.cpp
+++ 1/Programa1.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 using namespace std;
 int main() {
-	cout << "Hola" << endl;
-	cout << "Hola como estas" << endl;
-	cout << "Hola como estas12" << endl;
-	cout << "Hola como estas12312" << endl;
+
 }
